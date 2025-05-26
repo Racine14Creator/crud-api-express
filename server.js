bd0a7c866de2f3a8758dbc4522988387cf3c04de
bisimwa-14 <bchiruza@rmt-labs.com>
2025-05-26 20:08:59 +0200

    Changes

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors())
+
 app.use(express.json())
+
 app.use(express.urlencoded({extended: true}))
 
 app.use("/api/v1/users", userRoute);
