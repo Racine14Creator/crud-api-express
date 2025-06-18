b68549b5d1e6c1d88d60a47b2079c9dc9e56042a
bisimwa-14 <bchiruza@rmt-labs.com>
2025-06-18 08:23:58 +0200

    The server config

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import express from "express"
-import cors from "cors"
-import * as dotenv from "dotenv"
-import userRoute from "./routers/user.route.js"
+import cors from "cors";
+import express from "express";
+import * as dotenv from "dotenv";
+import userRoute from "./routers/user.route.js";
 
-dotenv.config({path: "./.env"})
+dotenv.config({path: "./.env"});
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 
 app.use("/api/v1/users", userRoute);
 
